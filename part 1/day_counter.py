1f0b5bf5fe700a70a62f1062a7c10b92c746a1b7
Ivan Zarichnyi <91795849+Pug10rd@users.noreply.github.com>
2024-06-22 14:20:54 +0200

    p-1 error handling fix

--- part 1/day_counter.py
+++ part 1/day_counter.py
@@ -4,11 +4,11 @@ from datetime import datetime
 def get_days_from_today(date=input("Print date in format: YYYY-MM-DD \n")):
     now = datetime.now()
     date_to_timedate = datetime.strptime(date, "%Y-%m-%d")
-    result = now - date_to_timedate
-
-    print(result.days)
-    return result.days
-
+    if date_to_timedate:
+        result = now - date_to_timedate
+        return print(result.days)
+    else:
+        print("Please check your date, it should be in format: YYYY-MM-DD")
 
 get_days_from_today()
 
